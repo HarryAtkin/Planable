8b1c0db56da366bac258ad8f27931132e94cdf6d
harry <harry@DESKTOP-19LCM1Q>
2024-01-30 16:51:42 +0000

    Add project files.

---
+++ Planable/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Planable
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
